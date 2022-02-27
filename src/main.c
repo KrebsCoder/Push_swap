4c4cfc96115c8f147b6ab7d5547f47b266323390
gabrielasertori <gabriela@sertori.com.br>
2022-02-27 02:14:42 +0100

    insert init and print stacks

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 01:04:07 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/02/26 23:48:11 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/02/27 02:09:27 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,15 @@ int	main(int argc, char *argv[])
 {
 	t_data data;
 
-	check(&data, argc, argv);
+	if (check(argc, argv) || init(&data, argc, argv))
+		return (ERROR);
 
+	// printa stacks na tela
+	for (int i = 0; i < argc; i++)
+		printf("%d  %d\n", data.stack_a[i], data.stack_b[i]);
+	printf("_  _\n");
+	printf("a  b\n");
+
+	free(data.stack_a);
 	return (0);
 }
