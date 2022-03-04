0b540bfd2af4e77ec227c032915cf6994b0e41cb
KrebsCoder <lucasklopess@gmail.com>
2022-03-04 17:29:56 -0300

    fixed norm

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 01:04:07 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/02/27 02:09:27 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/03/04 17:21:17 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,15 @@
 
 int	main(int argc, char *argv[])
 {
-	t_data data;
+	t_data	data;
 
 	if (check(argc, argv) || init(&data, argc, argv))
 		return (ERROR);
-
 	// printa stacks na tela
 	for (int i = 0; i < argc; i++)
 		printf("%d  %d\n", data.stack_a[i], data.stack_b[i]);
 	printf("_  _\n");
 	printf("a  b\n");
-
 	free(data.stack_a);
 	return (0);
 }
