1446974abdf41fa6adef992202f3a97b656c4905
KrebsCoder <lucasklopess@gmail.com>
2022-03-04 19:52:29 -0300

    removed /0 at end of int *

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 16:37:26 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/04 19:22:12 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/03/04 19:51:44 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,16 @@
 
 int	init(t_data *data, int argc, char *argv[])
 {
-	int	number;
 	int	i;
 
 	data->qnt_int = argc;
 	data->stack_a = (int *)malloc(argc * sizeof(int));
 	if (!data->stack_a)
 		return (FALSE);
-	data->stack_a[argc] = '\0';
 	i = 0;
 	while (i < argc)
 	{
-		number = ft_atoi(argv[i + 1]);
-		data->stack_a[i] = number;
+		data->stack_a[i] = ft_atoi(argv[i + 1]);
 		i++;
 	}
 	return (TRUE);
