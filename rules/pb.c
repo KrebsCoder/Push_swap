cced8dc60ba7473e3f8f62b0945a794b90c5f7e1
gabrielasertori <gabriela@sertori.com.br>
2022-03-08 04:08:21 +0100

    modify: add new malloc and free on aux_array

--- rules/pb.c
+++ rules/pb.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pb.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 16:24:31 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/08 03:35:43 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/03/08 04:06:10 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	pb(t_data *data)
 	int	i;
 
 	len = array_len(data->stack_b);
+	aux_array = malloc(sizeof(int) * len);
 	ft_memcpy(aux_array, data->stack_b, len);
 	free(data->stack_b);
 	data->stack_b = malloc(sizeof(int) * (len + 1));
@@ -30,4 +31,5 @@ void	pb(t_data *data)
 		i++;
 	}
 	data->stack_a = shift(data->stack_a);
+	free(aux_array);
 }
