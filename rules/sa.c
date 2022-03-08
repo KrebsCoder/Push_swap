286b7f93effca03e2e93becf0097280969f6b3ef
KrebsCoder <lkrebs-l@student.42sp.org.br>
2022-03-08 02:05:39 +0100

    removed unnecessary prints and fixed norm

--- rules/sa.c
+++ rules/sa.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sa.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 16:26:20 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/08 01:50:18 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/03/08 02:05:12 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,10 @@ void	sa(t_data *data)
 	int	len;
 
 	len = data->qnt_int;
-	printf("sizeof array: %d\n", len);
-
 	if (len <= 1)
 		return ;
 	temp = data->stack_a[0];
 	data->stack_a[0] = data->stack_a[1];
 	data->stack_a[1] = temp;
 	write(1, "sa\n", 3);
-
-	/* for temporario */
-	for (int i = 0; i < len; i++)
-		printf("%d", data->stack_a[i]);
-	printf("\n");
 }
-
