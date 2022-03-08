c95f1f836a1e0506d1df8058aa1dddc97586d0be
KrebsCoder <lkrebs-l@student.42sp.org.br>
2022-03-08 03:36:21 +0100

    add: pb rule is done

--- rules/pb.c
+++ rules/pb.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pb.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 16:24:31 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/02/26 16:24:52 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/03/08 03:35:43 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 void	pb(t_data *data)
 {
+	int	len;
+	int	*aux_array;
+	int	i;
 
+	len = array_len(data->stack_b);
+	ft_memcpy(aux_array, data->stack_b, len);
+	free(data->stack_b);
+	data->stack_b = malloc(sizeof(int) * (len + 1));
+	i = 0;
+	data->stack_b[0] = data->stack_a[0];
+	while (i < len)
+	{
+		data->stack_b[i + 1] = aux_array[i];
+		i++;
+	}
+	data->stack_a = shift(data->stack_a);
 }
