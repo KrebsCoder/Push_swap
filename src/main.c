bd99eae39a20b1e716bacfa6a3b4dc8adde3b8ef
gabrielasertori <gabriela@sertori.com.br>
2022-03-09 03:46:59 +0100

    modify: change print stack visibility

--- src/main.c
+++ src/main.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 01:04:07 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/08 02:55:22 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/03/09 03:28:19 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-static void	print_stack(int argc, t_data *data)
+void	print_stack(int argc, t_data *data)
 {
 	for (int i = 0; i < argc - 1; i++)
 		printf("%d  %d\n", data->stack_a[i], data->stack_b[i]);
@@ -24,6 +24,5 @@ int	main(int argc, char *argv[])
 {
 	t_data	data;
 
-	print_stack(argc, &data);
 	return (init_sort(&data, argc, argv));
 }
