fb1734fc5db94978cf093305b0ca0ae657e42ea9
gabrielasertori <gabriela@sertori.com.br>
2022-03-09 03:43:29 +0100

    add: add function realloc and arraycpy and now is working clean

--- rules/pb.c
+++ rules/pb.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 16:24:31 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/08 04:06:10 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/03/09 03:36:28 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,12 @@ void	pb(t_data *data)
 
 	len = array_len(data->stack_b);
 	aux_array = malloc(sizeof(int) * len);
-	ft_memcpy(aux_array, data->stack_b, len);
-	free(data->stack_b);
-	data->stack_b = malloc(sizeof(int) * (len + 1));
-	i = 0;
+	ft_arraycpy(aux_array, data->stack_b, len);
+	// for (int i = 0; i < 6 - 1; i++)
+	// 	printf("%d\n", aux_array[i]);
+	data->stack_b = ft_realloc(data->stack_b, (sizeof(int) * (len + 1)));
 	data->stack_b[0] = data->stack_a[0];
+	i = 0;
 	while (i < len)
 	{
 		data->stack_b[i + 1] = aux_array[i];
@@ -33,3 +34,25 @@ void	pb(t_data *data)
 	data->stack_a = shift(data->stack_a);
 	free(aux_array);
 }
+
+// void	pb(t_data *data)
+// {
+// 	int	len;
+// 	int	*aux_array;
+// 	int	i;
+
+// 	len = array_len(data->stack_b);
+// 	aux_array = malloc(sizeof(int) * len);
+// 	ft_memcpy(aux_array, data->stack_b, len);
+// 	free(data->stack_b);
+// 	data->stack_b = malloc(sizeof(int) * (len + 1));
+// 	i = 0;
+// 	data->stack_b[0] = data->stack_a[0];
+// 	while (i < len)
+// 	{
+// 		data->stack_b[i + 1] = aux_array[i];
+// 		i++;
+// 	}
+// 	data->stack_a = shift(data->stack_a);
+// 	free(aux_array);
+// }
