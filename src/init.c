3404e785c638b4812d7bc857f10e2cbbf11f4026
KrebsCoder <lucasklopess@gmail.com>
2022-03-09 21:20:45 -0300

    add: now it initializes the len_a and len_b

--- src/init.c
+++ src/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 16:37:26 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/09 03:26:13 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/03/09 21:15:33 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int	init(t_data *data, int argc, char *argv[])
 
 	data->stack_b = malloc(1 * sizeof(int));
 	data->stack_a = malloc(argc * sizeof(int));
+	data->len_a = argc;
+	data->len_b = 0;
 	if (!data->stack_a || !data->stack_b)
 		return (FALSE);
 	i = 0;
