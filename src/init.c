3666ab819f3adf1a59e0c70d069863502e68d645
KrebsCoder <lucasklopess@gmail.com>
2022-03-09 22:43:36 -0300

    modify: changed len_a = argc to len_a = argc - 1

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 16:37:26 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/09 21:15:33 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/03/09 21:38:09 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	init(t_data *data, int argc, char *argv[])
 
 	data->stack_b = malloc(1 * sizeof(int));
 	data->stack_a = malloc(argc * sizeof(int));
-	data->len_a = argc;
+	data->len_a = argc - 1;
 	data->len_b = 0;
 	if (!data->stack_a || !data->stack_b)
 		return (FALSE);
