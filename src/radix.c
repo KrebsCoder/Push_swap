afb981ca242f4826fa7fcbcb7ba89bb50bdc4edc
gabrielasertori <gabriela@sertori.com.br>
2022-03-10 04:00:44 +0100

    add: new functions pa and pb

--- src/radix.c
+++ src/radix.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 02:27:30 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/10 03:29:13 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/03/10 03:55:51 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 int	radix(t_data *data)
 {
 	pb(data);
-	pb(data);
-	sa(data);
-	sb(data);
-	//pa(data);
+	// pa(data);
+	// pb(data);
+	// sa(data);
+	// sb(data);
 	printf("\n\n\n\n");
 	print_stack(data->len_a, data->stack_a);
 	print_stack(data->len_b, data->stack_b);
