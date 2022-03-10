237d1b8590717a17ba4a932678109f5e5a30847f
KrebsCoder <lucasklopess@gmail.com>
2022-03-09 21:52:02 -0300

    error: needs to be fixed

--- rules/pb.c
+++ rules/pb.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 16:24:31 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/09 21:17:46 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/03/09 21:51:22 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,24 @@ void	pb(t_data *data)
 	int	*aux_array;
 	int	i;
 
-	aux_array = malloc(sizeof(int) * data->len_b);
-	ft_arraycpy(aux_array, data->stack_b, data->len_b);
-	data->stack_b = ft_realloc(data->stack_b, (sizeof(int) * (data->len_b + 1)));
-	data->stack_b[0] = data->stack_a[0];
-	i = 0;
-	while (i < data->len_b)
+	printf("len a:%d len b: %d", data->len_a, data->len_b);
+	aux_array = malloc(data->len_b * sizeof(int));
+	if (aux_array)
 	{
-		data->stack_b[i + 1] = aux_array[i];
-		i++;
+		ft_arraycpy(aux_array, data->stack_b, data->len_b);
+		data->stack_b = ft_realloc(data->stack_b, (sizeof(int) * (data->len_b + 1)));
+		data->stack_b[0] = data->stack_a[0];
+		i = 0;
+		while (i < data->len_b)
+		{
+			data->stack_b[i + 1] = aux_array[i];
+			i++;
+		}
 	}
+	data->stack_b[0] = data->stack_a[0];
+	data->len_a--;
+	data->len_b++;
 	data->stack_a = shift(data->stack_a, data->len_a);
-	data->len_a -= 1;
-	data->len_b += 1;
 	write(1, "pb\n", 3);
 	free(aux_array);
 }
