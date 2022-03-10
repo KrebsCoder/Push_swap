abecce0bc6b860f21415b1c30889672f90f8d29c
gabrielasertori <gabriela@sertori.com.br>
2022-03-10 03:58:38 +0100

    add: new pa function

--- rules/pa.c
+++ rules/pa.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pa.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 16:23:19 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/09 21:17:38 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/03/10 03:40:52 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,24 @@ void	pa(t_data *data)
 	int	*aux_array;
 	int	i;
 
-	aux_array = malloc(sizeof(int) * data->len_a);
-	ft_arraycpy(aux_array, data->stack_a, data->len_a);
-	data->stack_a = ft_realloc(data->stack_a, (sizeof(int) * (data->len_a + 1)));
-	data->stack_a[0] = data->stack_b[0];
-	i = 0;
-	while (i < data->len_a)
+	aux_array = malloc(data->len_a * sizeof(int));
+	if ((data->len_a * sizeof(int)) != 0)
 	{
-		data->stack_a[i + 1] = aux_array[i];
-		i++;
+		ft_arraycpy(aux_array, data->stack_a, data->len_a);
+		data->stack_a = ft_realloc(data->stack_a, (sizeof(int) * (data->len_a + 1)));
+		data->stack_a[0] = data->stack_b[0];
+		i = 0;
+		while (i < data->len_a)
+		{
+			data->stack_a[i + 1] = aux_array[i];
+			i++;
+		}
 	}
+	else
+		data->stack_a[0] = data->stack_b[0];
 	data->stack_b = shift(data->stack_b, data->len_b);
-	data->len_a += 1;
-	data->len_b -= 1;
+	data->len_a++;
+	data->len_b--;
 	write(1, "pa\n", 3);
 	free(aux_array);
 }
