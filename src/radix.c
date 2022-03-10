5c933315425abfea01e61030f20da4530e503cdb
gabrielasertori <gabriela@sertori.com.br>
2022-03-10 03:33:25 +0100

    add: frees

--- src/radix.c
+++ src/radix.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   radix.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 02:27:30 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/09 21:33:22 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/03/10 03:29:13 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,14 @@
 int	radix(t_data *data)
 {
 	pb(data);
+	pb(data);
+	sa(data);
+	sb(data);
 	//pa(data);
 	printf("\n\n\n\n");
 	print_stack(data->len_a, data->stack_a);
 	print_stack(data->len_b, data->stack_b);
+	free(data->stack_a);
+	free(data->stack_b);
 	return (EXIT_SUCCESS);
 }
