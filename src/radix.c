d75f002309928988322b1f180ee4d7e875fd68f6
KrebsCoder <lucasklopess@gmail.com>
2022-03-12 11:43:48 -0300

    add: now all frees are called by ft_free function

--- src/radix.c
+++ src/radix.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   radix.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 02:27:30 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/12 02:28:19 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/03/12 11:08:02 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ int	radix(t_data *data)
 	normalize(data);
 	printf("\n\ndepois: \n");
 	print_stack(data->len_a, data->stack_a);
-	free(data->copy_stack);
-	free(data->stack_a);
-	free(data->stack_b);
+	ft_free(data);
 	return (EXIT_SUCCESS);
 }
