6d0990cda909bda6d44b3dc8481f537a568f9be1
KrebsCoder <lucasklopess@gmail.com>
2022-03-12 11:43:21 -0300

    add: small_sort_3 function

--- src/init_sort.c
+++ src/init_sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_sort.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 02:36:40 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/12 02:29:44 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/03/12 11:32:16 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,16 @@ int	init_sort(t_data *data, int argc, char *argv[])
 {
 	if (!check(argc, argv) || !init(data, argc, argv))
 		return (EXIT_FAILURE);
-	if (argc >= 6)
+	if (data->len_a <= 3)
+	{
+		printf("before\n\n");
+		print_stack(data->len_a, data->stack_a);
+		small_sort_3(data);
+		printf("after\n\n");
+		print_stack(data->len_a, data->stack_a);
+		return (EXIT_SUCCESS);
+	}
+	else if (argc >= 6)
 		return (radix(data));
-	radix(data);
 	return (EXIT_SUCCESS);
 }
