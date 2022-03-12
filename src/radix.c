4973be29b3910e7bb732a66b8ffcedc835f60030
gabrielasertori <gabriela@sertori.com.br>
2022-03-12 02:46:47 +0100

    modify: main to studies purpose

--- src/radix.c
+++ src/radix.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 02:27:30 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/10 03:55:51 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/03/12 02:28:19 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 int	radix(t_data *data)
 {
-	pb(data);
-	// pa(data);
-	// pb(data);
-	// sa(data);
-	// sb(data);
-	printf("\n\n\n\n");
+	printf("antes: \n");
 	print_stack(data->len_a, data->stack_a);
-	print_stack(data->len_b, data->stack_b);
+	normalize(data);
+	printf("\n\ndepois: \n");
+	print_stack(data->len_a, data->stack_a);
+	free(data->copy_stack);
 	free(data->stack_a);
 	free(data->stack_b);
 	return (EXIT_SUCCESS);
