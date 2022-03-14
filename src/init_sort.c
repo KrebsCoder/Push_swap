182d41ff25c2608c2a454f7c3535a90e7fbdf728
gabrielasertori <gabriela@sertori.com.br>
2022-03-14 04:14:55 +0100

    modify: refactoring init_sort

--- src/init_sort.c
+++ src/init_sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_sort.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 02:36:40 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/12 11:32:16 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/03/14 03:35:56 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,12 @@ int	init_sort(t_data *data, int argc, char *argv[])
 {
 	if (!check(argc, argv) || !init(data, argc, argv))
 		return (EXIT_FAILURE);
+	else
+		normalize(data);
 	if (data->len_a <= 3)
-	{
-		printf("before\n\n");
-		print_stack(data->len_a, data->stack_a);
-		small_sort_3(data);
-		printf("after\n\n");
-		print_stack(data->len_a, data->stack_a);
-		return (EXIT_SUCCESS);
-	}
-	else if (argc >= 6)
+		return (small_sort_3(data));
+	else if (data->len_a <= 5)
+		return (small_sort_5(data));
+	else
 		return (radix(data));
-	return (EXIT_SUCCESS);
 }
