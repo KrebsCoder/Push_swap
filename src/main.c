d173a1c66b87108f83f182e85415805e1dc0295f
gabrielasertori <gabriela@sertori.com.br>
2022-03-14 04:28:14 +0100

    mod: test

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 01:04:07 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/10 03:27:34 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/03/14 04:27:46 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	print_stack(int argc, int array[])
 	for (int i = 0; i < argc; i++)
 		printf("%d\n", array[i]);
 	printf("-\n");
-	printf("x\n\n");
+	printf("q\n\n");
 }
 
 
