e66e0350fe3d5c0272ef2e3090d73033ff70ea0c
gabrielasertori <gabriela@sertori.com.br>
2022-03-16 04:21:14 +0100

    fix: function find_min_int and swap b changed

--- src/small_sort.c
+++ src/small_sort.c
@@ -6,13 +6,13 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 10:44:05 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/03/16 03:07:17 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/03/16 04:19:28 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-static int	find_max_int(t_data *data);
+static int	find_min_int(t_data *data);
 static	void push_b(t_data *data, int index, int mid_index, int last_index);
 static	void sort_b(t_data *data);
 
@@ -40,46 +40,44 @@ int	small_sort_5(t_data *data)
 	int	index;
 	int	i;
 
-	// print_stack(data->len_a, data->stack_a);
+	print_stack(data->len_a, data->stack_a);
 	// 12  -  4 3 5 2 1
 
 	i = 0;
 	while (i < 2)
 	{
-		index = find_max_int(data);
+		index = find_min_int(data);
 		if (i == 0)
 			push_b(data, index, 2, 4);
 		else
 			push_b(data, index, 1, 3);
-		// printf("stack a %d passada: \n", i+1);
-		// print_stack(data->len_a, data->stack_a);
-		// printf("stack b %d passada: \n", i+1);
-		// print_stack(data->len_b, data->stack_b);
+		printf("stack a %d passada: \n", i+1);
+		print_stack(data->len_a, data->stack_a);
+		printf("stack b %d passada: \n", i+1);
+		print_stack(data->len_b, data->stack_b);
 		i++;
 	}
 	sort_b(data);
 	small_sort_3(data);
 	pa(data);
 	pa(data);
-	ra(data);
-	ra(data);
 
-	// printf("Maior indice: %d\n", index);
-	// print_stack(data->len_a, data->stack_a);
-	// print_stack(data->len_b, data->stack_b);
+	printf("Maior indice: %d\n", index);
+	print_stack(data->len_a, data->stack_a);
+	print_stack(data->len_b, data->stack_b);
 	return (EXIT_SUCCESS);
 }
 
-static int	find_max_int(t_data *data)
+static int	find_min_int(t_data *data)
 {
 	int	i;
 	int	index;
 
-	i = 0;
+	i = 1;
 	index = 0;
 	while (i <= data->len_a)
 	{
-		if (data->stack_a[index] < data->stack_a[i])
+		if (data->stack_a[index] > data->stack_a[i])
 			index = i;
 		i++;
 	}
@@ -114,6 +112,6 @@ static	void push_b(t_data *data, int index, int mid_index, int last_index)
 
 static	void sort_b(t_data *data)
 {
-	if (data->stack_b[0] > data->stack_b[1])
+	if (data->stack_b[0] < data->stack_b[1])
 		sb(data);
 }
