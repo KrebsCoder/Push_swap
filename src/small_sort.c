9e87474c452c3067f6ffb7de8495a6159469cea7
gabrielasertori <gabriela@sertori.com.br>
2022-03-16 03:08:37 +0100

    add: new functions and add logic to small sort 5

--- src/small_sort.c
+++ src/small_sort.c
@@ -6,11 +6,15 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 10:44:05 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/03/14 04:06:55 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/03/16 03:07:17 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
+
+static int	find_max_int(t_data *data);
+static	void push_b(t_data *data, int index, int mid_index, int last_index);
+static	void sort_b(t_data *data);
 
 int	small_sort_3(t_data *data)
 {
@@ -36,9 +40,42 @@ int	small_sort_5(t_data *data)
 	int	index;
 	int	i;
 
-	print_stack(data->len_a, data->stack_a);
+	// print_stack(data->len_a, data->stack_a);
+	// 12  -  4 3 5 2 1
 
-	i = 1;
+	i = 0;
+	while (i < 2)
+	{
+		index = find_max_int(data);
+		if (i == 0)
+			push_b(data, index, 2, 4);
+		else
+			push_b(data, index, 1, 3);
+		// printf("stack a %d passada: \n", i+1);
+		// print_stack(data->len_a, data->stack_a);
+		// printf("stack b %d passada: \n", i+1);
+		// print_stack(data->len_b, data->stack_b);
+		i++;
+	}
+	sort_b(data);
+	small_sort_3(data);
+	pa(data);
+	pa(data);
+	ra(data);
+	ra(data);
+
+	// printf("Maior indice: %d\n", index);
+	// print_stack(data->len_a, data->stack_a);
+	// print_stack(data->len_b, data->stack_b);
+	return (EXIT_SUCCESS);
+}
+
+static int	find_max_int(t_data *data)
+{
+	int	i;
+	int	index;
+
+	i = 0;
 	index = 0;
 	while (i <= data->len_a)
 	{
@@ -46,32 +83,37 @@ int	small_sort_5(t_data *data)
 			index = i;
 		i++;
 	}
-	if (index == 0)
-		pb(data);
-	else if (index == 1)
+	return (index);
+}
+
+static	void push_b(t_data *data, int index, int mid_index, int last_index)
+{
+	int	i;
+
+	i = 0;
+	while (i < index + 1)
 	{
-		ra(data);
-		pb(data);
+		if (index <= mid_index)
+		{
+			if (index == 0)
+				break ;
+			ra(data);
+			i++;
+		}
+		else
+		{
+			rra(data);
+			if (index == last_index)
+				break ;
+			i++;
+		}
+		i++;
 	}
-	else if (index == 2)
-	{
-		ra(data);
-		ra(data);
-		pb(data);
-	}
-	else if (index == 3)
-	{
-		rra(data);
-		rra(data);
-		pb(data);
-	}
-	else if (index == 4)
-	{
-		rra(data);
-		pb(data);
-	}
-	printf("Maior indice: %d\n", index);
-	print_stack(data->len_a, data->stack_a);
-	print_stack(data->len_b, data->stack_b);
-	return (EXIT_SUCCESS);
+	pb(data);
+}
+
+static	void sort_b(t_data *data)
+{
+	if (data->stack_b[0] > data->stack_b[1])
+		sb(data);
 }
