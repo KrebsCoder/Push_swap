87da31a2223747ddf3e9d4f7162d6d2d08a9dc73
KrebsCoder <lucasklopess@gmail.com>
2022-03-17 16:41:12 -0300

    remove: removed free on copy_stack

--- src/free.c
+++ src/free.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 11:06:13 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/03/12 11:07:48 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/03/17 16:40:00 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 void	ft_free(t_data *data)
 {
-	free(data->copy_stack);
 	free(data->stack_a);
 	free(data->stack_b);
 }
