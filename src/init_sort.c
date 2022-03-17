9c7eccd4aca10509822f1a003b656dbdd32cfe78
KrebsCoder <lucasklopess@gmail.com>
2022-03-16 23:44:06 -0300

    modify: organization is better

--- src/init_sort.c
+++ src/init_sort.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 02:36:40 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/16 23:40:32 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/03/16 23:43:22 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,11 @@ int	init_sort(t_data *data, int argc, char *argv[])
 	else
 		normalize(data);
 	if (data->len_a <= 3)
-	{
 		exit = small_sort_3(data, 1, 2, 3);
-		ft_free(data);
-		return (exit);
-	}
 	else if (data->len_a <= 5)
-	{
 		exit = small_sort_5(data);
-		ft_free(data);
-		return (exit);
-	}
 	else
-		return (radix(data));
+		exit = radix(data);
+	ft_free(data);
+	return (exit);
 }
