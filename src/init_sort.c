96d8e05f6389225c37d866fc0e6d0dbbec2fe48d
KrebsCoder <lucasklopess@gmail.com>
2022-03-16 23:38:22 -0300

    fix: memory leak on small_sort_3

--- src/init_sort.c
+++ src/init_sort.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 02:36:40 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/16 22:34:09 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/03/16 23:31:33 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 
 int	init_sort(t_data *data, int argc, char *argv[])
 {
+	int	exit;
 	if (!check(argc, argv) || !init(data, argc, argv))
 		return (EXIT_FAILURE);
 	else
 		normalize(data);
 	if (data->len_a <= 3)
-		return (small_sort_3(data, 1, 2, 3));
+	{
+		exit = small_sort_3(data, 1, 2, 3);
+		ft_free(data);
+		return (exit);
+	}
 	else if (data->len_a <= 5)
 		return (small_sort_5(data));
 	else
